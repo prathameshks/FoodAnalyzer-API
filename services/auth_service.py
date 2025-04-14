2b9f762e5f7e5e04e30e3e9af583a24c1679f75f
Prathamesh Sable <82529994+prathameshks@users.noreply.github.com>
2025-04-14 16:02:30 +0530

    bug fix

--- services/auth_service.py
+++ services/auth_service.py
@@ -64,7 +64,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
         if expires_delta:
             expire = datetime.utcnow() + expires_delta
         else:
-            expire = datetime.utcnow() + timedelta(days==15)
+            expire = datetime.utcnow() + timedelta(days=15)
         to_encode.update({"exp": expire})
         encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
         return encoded_jwt
