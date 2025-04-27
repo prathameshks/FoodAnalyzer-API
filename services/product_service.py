555cc463363e2b1341afd208c61515062d4a5162
prathameshks2003 <prathameshks2003@gmail.com>
2025-04-27 07:06:05 +0000

    product model update

--- services/product_service.py
+++ services/product_service.py
@@ -1,14 +1,16 @@
 from sqlalchemy.orm import Session
 from typing import Optional
 from db.models import Product
+from interfaces.productModels import ProductCreate
 
 class ProductService:
     def __init__(self, db: Session):
         self.db = db
 
-    def add_product(self, name: str, ingredients_text: str) -> Product:
-        product = Product(product_name=name, ingredients_text=ingredients_text)
-        self.db.add(product)
+    def add_product(self, product_create: ProductCreate) -> Product:
+        product = Product(**product_create.model_dump())
+        
+        self.db.add(product)        
         self.db.commit()
         self.db.refresh(product)
         return product
